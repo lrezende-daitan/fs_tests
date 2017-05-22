24d79bf200217e697c4e45072c59454ad09cf1e6
Luciano Rezende <lrezende@daitangroup.com>
2017-05-22 16:07:24 -0300

    Split dialplan.xml into one file per extension

--- dialplan/app.js
+++ dialplan/app.js
@@ -1,16 +1,26 @@
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
+const app = express();
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
 app.post("/dialplan.xml", function(req, res) {
+    let requestInfo = req.body;
     console.log("\n==================================\n");
-    console.log(req.body);
-    res.download(path.resolve("./dialplan.xml"));
+    console.log("Core-UUID: " + requestInfo["Core-UUID"] + "\n");
+    console.log("Call UUID: " + requestInfo["Channel-Call-UUID"] + "\n");
+    console.log("Caller: " + requestInfo["Caller-Username"] + "\n");
+    console.log("Destination: " + requestInfo["Caller-Destination-Number"] + "\n");
+    let filePath = "./extensions/" + requestInfo["Caller-Destination-Number"] + ".xml";
+    if(!fs.existsSync(filePath)) {
+      filePath = "./extensions/generic.xml";
+    }
+    console.log("Returning " + filePath);
+    return res.download(path.resolve(filePath));
 });
  
 var server = app.listen(3000, function () {
