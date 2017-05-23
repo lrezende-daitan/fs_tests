cfd3de8cab7312068ee7e8a44f628b24e3654077
Arthur Geron <arthurgeron@hotmail.com>
2017-05-23 14:49:06 -0300

    Added strict mode to allow using of block-scoped declarations.

--- dialplan/app.js
+++ dialplan/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
 app.post("/dialplan.xml", function(req, res) {
+    "use strict";
     let requestInfo = req.body;
     console.log("\n==================================\n");
     console.log("Core-UUID: " + requestInfo["Core-UUID"]);
